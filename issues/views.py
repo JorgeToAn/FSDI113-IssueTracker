78bfa8596302ad4ef46f20fa8ac138324f6fb289
JorgeToAn <jorgetosta.f@gmail.com>
2022-10-05 01:49:23 -0700

    MVP

--- issues/views.py
+++ issues/views.py
@@ -1,10 +1,11 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Issue, Status
 
 
-class IssueContextListView(ListView):
+class IssueContextListView(LoginRequiredMixin, ListView):
     template_name: str='issues/list.html'
     model = Issue
 
@@ -17,36 +18,66 @@ class IssueContextListView(ListView):
                                         "created_on")
         return context
 
-class IssueListView(ListView):
+class IssueListView(LoginRequiredMixin, ListView):
     template_name: str='issues/list.html'
     model = Issue
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issue_list'] = Issue.objects.filter(
+                                requester=self.request.user).order_by(
+                                        "created_on")
+        return context
+
 class IssueToDoListView(IssueContextListView):
+    status = "To Do"
+
     def get_context_data(self):
-        return super().get_context_data(status_name="To Do")
+        return super().get_context_data(status_name=self.status)
 
 class IssueInProgressListView(IssueContextListView):
+    status = "In Progress"
+
     def get_context_data(self):
-        return super().get_context_data(status_name="In Progress")
+        return super().get_context_data(status_name=self.status)
 
 class IssueDoneListView(IssueContextListView):
-    def get_context_data(self):
-        return super().get_context_data(status_name="Done")
+    status = "Done"
 
-class IssueDetailView(DetailView):
+    def get_context_data(self):
+        return super().get_context_data(status_name=self.status)
+
+class IssueDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     template_name: str='issues/detail.html'
     model = Issue
 
-class IssueCreateView(CreateView):
+    def test_func(self):
+        issue_obj = self.get_object()
+        return issue_obj.requester == self.request.user
+
+class IssueCreateView(LoginRequiredMixin, CreateView):
     template_name: str='issues/new.html'
     model = Issue
     fields = ['title', 'summary', 'description', 'assignee', 'status']
 
-class IssueUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.requester = self.request.user
+        return super().form_valid(form)
+
+class IssueUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name: str='issues/update.html'
     model = Issue
     fields = ['title', 'summary', 'description', 'assignee', 'status']
 
-class IssueDeleteView(DeleteView):
+    def test_func(self):
+        issue_obj = self.get_object()
+        return issue_obj.requester == self.request.user
+
+class IssueDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name: str='issues/delete.html'
     model = Issue
+    success_url: reverse_lazy('list')
+
+    def test_func(self):
+        issue_obj = self.get_object()
+        return issue_obj.requester == self.request.user
